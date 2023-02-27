1bb6ea60d69627917bddc79b33ee2710fd18bd06
Kisna Lunagariya <125738423+Kisna28@users.noreply.github.com>
2023-02-27 23:01:13 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/glide/MainActivity.java
+++ app/src/main/java/com/example/glide/MainActivity.java
@@ -36,19 +36,19 @@ public class MainActivity extends AppCompatActivity {
     }
     List<Data>getinformation(){
         List<Data> dataList = new ArrayList<>();
-dataList.add(new Data("Kisna","https://rukminim1.flixcart.com/image/416/416/kzn17680/computer/1/i/z/-original-imagbhzqahdzyx4e.jpeg?q=70"));
-dataList.add(new Data("hgfdh","https://rukminim1.flixcart.com/image/416/416/xif0q/computer/8/l/f/-original-imagg2ckcragzhhe.jpeg?q=70"));
-dataList.add(new Data("ghdf","https://rukminim1.flixcart.com/image/416/416/l2qhjm80/computer/p/i/1/crosshair-15-b12uez-gaming-laptop-msi-original-imageymuhhs7djjf.jpeg?q=70"));
-dataList.add(new Data("Kigfdhsna","https://rukminim1.flixcart.com/image/416/416/l0vbukw0/computer/z/o/e/-original-imagckcfz6jgjn3z.jpeg?q=70"));
-dataList.add(new Data("Kifghsna","https://rukminim1.flixcart.com/image/416/416/xif0q/computer/8/l/c/raider-ge77hx-12uhs-205in-gaming-laptop-msi-original-imaghffpfubseq3n.jpeg?q=70"));
-dataList.add(new Data("Kisgfdhna","https://rukminim1.flixcart.com/image/416/416/kkwwu4w0/mouse/2/h/g/g402-logitech-original-imagy5ny25h8nsms.jpeg?q=70"));
-dataList.add(new Data("Kidfhfghdhsna","https://rukminim1.flixcart.com/image/416/416/xif0q/keyboard/2/u/a/-original-imagn3f8ybfyxsrf.jpeg?q=70"));
-dataList.add(new Data("Khdghhgisna","https://rukminim1.flixcart.com/image/416/416/xif0q/headphone/n/e/0/e5000pro-eksa-original-imaggmshrzwnjchg.jpeg?q=70"));
-dataList.add(new Data("Kivdfgsna","https://rukminim1.flixcart.com/image/416/416/koenl3k0/cooling-pad/n/u/7/dcx-a101-lapcare-original-imag2vd7dpb8r5ff.jpeg?q=70"));
-dataList.add(new Data("Kifdgfdssna","https://rukminim1.flixcart.com/image/416/416/kpr8k280/mousepad/q/4/x/mp35st-redgear-original-imag3wzn8xhkgrz9.jpeg?q=70"));
-dataList.add(new Data("Kifdgdfhsna","https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/2/s/7/-original-imagmg6gktts6sfy.jpeg?q=70"));
-dataList.add(new Data("Kfhdfhisna","https://rukminim1.flixcart.com/image/416/416/ky7lci80/mobile/t/x/j/-original-imagahvge92r5fmm.jpeg?q=70"));
-dataList.add(new Data("Kfghfgdisna","https://rukminim1.flixcart.com/image/416/416/xif0q/headphone/l/j/e/-original-imagnfufffaczakh.jpeg?q=70"));
+dataList.add(new Data("Hp Omen","https://rukminim1.flixcart.com/image/416/416/kzn17680/computer/1/i/z/-original-imagbhzqahdzyx4e.jpeg?q=70"));
+dataList.add(new Data("Msi","https://rukminim1.flixcart.com/image/416/416/xif0q/computer/8/l/f/-original-imagg2ckcragzhhe.jpeg?q=70"));
+dataList.add(new Data("Msi","https://rukminim1.flixcart.com/image/416/416/l2qhjm80/computer/p/i/1/crosshair-15-b12uez-gaming-laptop-msi-original-imageymuhhs7djjf.jpeg?q=70"));
+dataList.add(new Data("Asus Rog","https://rukminim1.flixcart.com/image/416/416/l0vbukw0/computer/z/o/e/-original-imagckcfz6jgjn3z.jpeg?q=70"));
+dataList.add(new Data("Msi","https://rukminim1.flixcart.com/image/416/416/xif0q/computer/8/l/c/raider-ge77hx-12uhs-205in-gaming-laptop-msi-original-imaghffpfubseq3n.jpeg?q=70"));
+dataList.add(new Data("Gaming mouse","https://rukminim1.flixcart.com/image/416/416/kkwwu4w0/mouse/2/h/g/g402-logitech-original-imagy5ny25h8nsms.jpeg?q=70"));
+dataList.add(new Data("Gaming Keyboard","https://rukminim1.flixcart.com/image/416/416/xif0q/keyboard/2/u/a/-original-imagn3f8ybfyxsrf.jpeg?q=70"));
+dataList.add(new Data("Headphone","https://rukminim1.flixcart.com/image/416/416/xif0q/headphone/n/e/0/e5000pro-eksa-original-imaggmshrzwnjchg.jpeg?q=70"));
+dataList.add(new Data("Colling pad","https://rukminim1.flixcart.com/image/416/416/koenl3k0/cooling-pad/n/u/7/dcx-a101-lapcare-original-imag2vd7dpb8r5ff.jpeg?q=70"));
+dataList.add(new Data("Mouse pad","https://rukminim1.flixcart.com/image/416/416/kpr8k280/mousepad/q/4/x/mp35st-redgear-original-imag3wzn8xhkgrz9.jpeg?q=70"));
+dataList.add(new Data("Samsung s23","https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/2/s/7/-original-imagmg6gktts6sfy.jpeg?q=70"));
+dataList.add(new Data("Asus phone","https://rukminim1.flixcart.com/image/416/416/ky7lci80/mobile/t/x/j/-original-imagahvge92r5fmm.jpeg?q=70"));
+dataList.add(new Data("Boat gaming earbuds","https://rukminim1.flixcart.com/image/416/416/xif0q/headphone/l/j/e/-original-imagnfufffaczakh.jpeg?q=70"));
 
 return dataList;
 
